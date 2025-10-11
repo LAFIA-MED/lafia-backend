1706dd479bc0426c18fe4e3aa036533401347015
UltimateProdigy <ayobamiakinola84@gmail.com>
2025-10-11 12:56:35 -0700

    fix

--- routes/prescription.ts
+++ routes/prescription.ts
@@ -2,50 +2,60 @@ import { Router, Request, Response, NextFunction } from "express";
 import { requireRoles } from "../middleware/requireRoles";
 import { ROLE } from "@prisma/client";
 import { validateBody } from "../middleware/validateBody";
-import { createPrescription, getAllPrescriptions, getPrescriptionById } from "../services/prescriptionService";
-
+import {
+  createPrescription,
+  getAllPrescriptions,
+  getPrescriptionById,
+} from "../services/prescriptionService";
 
 const router = Router();
 
 router.get("/", async (req: Request, res: Response, next: NextFunction) => {
+  try {
+    const result = await getAllPrescriptions();
+    res.status(200).json({
+      success: true,
+      ...result,
+    });
+  } catch (error) {
+    next(error);
+  }
+});
+
+router.get(
+  "/:prescriptionId",
+  async (req: Request, res: Response, next: NextFunction) => {
     try {
-        const result = await getAllPrescriptions();
-        res.status(200).json({
-            success: true,
-            ...result,
-        });
-    } catch (error) {
-        next(error);
-    }
-})
+      const { prescriptionId } = req.params;
+      const prescription = await getPrescriptionById(prescriptionId);
 
-router.get("/:prescriptionId", async (req: Request, res: Response, next: NextFunction) => {
+      res.status(200).json({
+        success: true,
+        message: "Hospital retrieved successfully",
+        data: prescription,
+      });
+    } catch (error) {
+      next(error);
+    }
+  }
+);
+
+router.post(
+  "/",
+  requireRoles([ROLE.DOCTOR]),
+  async (req: Request, res: Response, next: NextFunction) => {
     try {
-        const { prescriptionId } = req.params;
-        const prescription = await getPrescriptionById(prescriptionId);
+      const prescription = await createPrescription(req.body);
 
-        res.status(200).json({
-            success: true,
-            message: "Hospital retrieved successfully",
-            data: prescription,
-        });
+      res.status(201).json({
+        success: true,
+        message: "Prescription created successfully",
+        data: prescription,
+      });
     } catch (error) {
-        next(error);
+      next(error);
     }
-})
-
-router.post("/", requireRoles([ROLE.DOCTOR]), async (req: Request, res: Response, next: NextFunction) => {
-    try {
-        const prescription = await createPrescription(req.body);
-
-        res.status(201).json({
-            success: true,
-            message: "Prescription created successfully",
-            data: prescription,
-        });
-    } catch (error) {
-        next(error);
-    }
-})
+  }
+);
 
 export { router as prescriptionRoutes };
